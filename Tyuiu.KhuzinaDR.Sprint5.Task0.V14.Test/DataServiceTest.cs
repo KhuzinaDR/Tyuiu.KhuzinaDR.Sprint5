c9d3bcb452633f93ab65030a3a6e311cdd1a2b78
angeldrip9919 <i61angel>
2025-01-24 03:22:43 +0500

    Add project files.

---
+++ Tyuiu.KhuzinaDR.Sprint5.Task0.V14.Test/DataServiceTest.cs
@@ -0,0 +1,21 @@
+using Tyuiu.KhuzinaDR.Sprint5.Task0.V14.Lib;
+
+namespace Tyuiu.KhuzinaDR.Sprint5.Task0.V14.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestSaveToFileTextData()
+        {
+            DataService ds = new DataService();
+            int x = 3;
+
+            string filePath = ds.SaveToFileTextData(x);
+            string result = File.ReadAllText(filePath);
+
+            Assert.IsTrue(File.Exists(filePath));
+            Assert.AreEqual("4,154", result);
+        }
+    }
+}
