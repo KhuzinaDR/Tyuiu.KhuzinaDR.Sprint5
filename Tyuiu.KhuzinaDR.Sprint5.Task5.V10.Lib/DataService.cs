e7873dcd406387a92da30d0ddb3728edbc94e312
Tyuiu.DolganovAA@outlook.com <Tyuiu.DolganovAA@outlook.com>
2025-01-24 17:32:21 +0300

    Task5

---
+++ Tyuiu.KhuzinaDR.Sprint5.Task5.V10.Lib/DataService.cs
@@ -0,0 +1,28 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint5;
+namespace Tyuiu.KhuzinaDR.Sprint5.Task5.V10.Lib
+{
+    public class DataService : ISprint5Task5V10
+    {
+        public double LoadFromDataFile(string path)
+        {
+            double res = 0;
+            using (StreamReader reader = new StreamReader(path))
+            {
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    string[] nums = line.Split(' ');
+                    foreach (string s in nums)
+                    {
+                        double x = double.Parse(s, System.Globalization.CultureInfo.InvariantCulture);
+                        if (x % 2 == 0)
+                        {
+                            res += x;
+                        }
+                    }
+                }
+            }
+            return res;
+        }
+    }
+}
---
+++ Tyuiu.KhuzinaDR.Sprint5.Task5.V10/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Tyuiu.KhuzinaDR.Sprint5.Task5.V10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
