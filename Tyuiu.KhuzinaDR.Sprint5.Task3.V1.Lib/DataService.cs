3519bcfc630be2c81f9a7ace2b22ebefb7a8445c
Tyuiu.DolganovAA@outlook.com <Tyuiu.DolganovAA@outlook.com>
2025-01-24 17:12:22 +0300

    Task3

--- Tyuiu.KhuzinaDR.Sprint5.Task3.V1.Lib/DataService.cs
+++ Tyuiu.KhuzinaDR.Sprint5.Task3.V1.Lib/DataService.cs
@@ -1,14 +1,18 @@
 ﻿using tyuiu.cources.programming.interfaces.Sprint5;
 using System.IO;
 using System.Text;
-namespace Tyuiu.KhuzinaDR.Sprint5.Task3.V15.Lib
+namespace Tyuiu.KhuzinaDR.Sprint5.Task3.V1.Lib
 {
-    public class DataService : ISprint5Task3V15
+    public class DataService : ISprint5Task3V1
     {
         public string SaveToFileTextData(int x)
         {
-            string path = Path.Combine(new string[] { Path.GetTempPath(), "OutPutFileTask3.bin" });
-            double y = Math.Round(-1.4 * Math.Pow(x, 3) + 2.3 * Math.Pow(x, 2) + 0.6 * x, 3);
+            string path = $@"{Path.GetTempPath()}OutPutFileTask3.bin";
+
+            double y = (Math.Pow(x, 3) - 8d) / (2d * Math.Pow(x, 2));
+
+            y = Math.Round(y, 3);
+
             using (BinaryWriter writer = new BinaryWriter(File.Open(path, FileMode.OpenOrCreate), Encoding.UTF8))
             {
                 writer.Write(BitConverter.GetBytes(y));
