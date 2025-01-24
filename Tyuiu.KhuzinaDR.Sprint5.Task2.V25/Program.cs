31b8d32c1a7b1d1939eaf710e0684eff5b64c688
Tyuiu.DolganovAA@outlook.com <Tyuiu.DolganovAA@outlook.com>
2025-01-24 16:49:44 +0300

    Task2

---
+++ Tyuiu.KhuzinaDR.Sprint5.Task2.V25/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Tyuiu.KhuzinaDR.Sprint5.Task2.V25
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
